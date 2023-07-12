b528ce7f5062feb594e28815b3ee353361613e3f
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-12 13:52:14 +0200

    desktop preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#FF385C",
+      },
+      fontFamily: {
+        main: "Air-bnb",
+        "main-medium": "Air-bnb-Medium",
+        "main-bold": "Air-bnb-Bold",
+      },
+      backgroundImage: {
+        "place1": "url(../assets/images/place-1.jpeg)"
+      }
+    },
   },
   plugins: [],
-}
+};
